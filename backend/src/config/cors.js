94d76cb8686fcd35edd8da262456b13a967412d2
anamika-zedni <anamikazedni@gamil.com>
2025-04-13 22:27:39 +0600

    fix cors issue

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,9 +1,21 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://172.174.245.134:3000",
+  "http://172.174.245.134",
+];
+
 const corsOptions = {
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['set-cookie']
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["set-cookie"],
 };
 
 module.exports = corsOptions;
